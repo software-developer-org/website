72537375551dff5b3b3220a72b039c9b14e00210
KlugeCh <christian.kluge@software-developer.org>
2021-03-17 08:46:14 +0100

    #6 fixed linter errors in app.js routes/index.js routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+// eslint-disable-next-line no-unused-vars
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	res.send('respond with a resource');
 });
 
 module.exports = router;
