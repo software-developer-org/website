ff23bb7defd5fca974af58685abff6e20a79e113
KlugeCh <christian.kluge@software-developer.org>
2021-03-17 11:52:25 +0100

    #6 corrected wrong header and title in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 // eslint-disable-next-line no-unused-vars
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Main Page' });
+	res.render('index', { title: 'Hello World' });
 });
 
 module.exports = router;
