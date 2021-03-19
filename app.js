17ba4a26fbcd1e3bb8af4e9f7e512a650446008f
RouvenGonzalez <rouven.gonzalez@software-developer.org>
2021-03-19 11:26:00 +0100

    #13 created router file for the about page andimplemented the setup for it in app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var aboutRouter = require('./routes/about');
 
 var app = express();
 
@@ -21,22 +22,23 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+ next(createError(404));
 });
 
 // error handler
 // eslint-disable-next-line no-unused-vars
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+ // set locals, only providing error in development
+ res.locals.message = err.message;
+ res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+ // render the error page
+ res.status(err.status || 500);
+ res.render('error');
 });
 
 module.exports = app;
