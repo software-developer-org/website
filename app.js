3b14f37381de8497181c690168e0b6da08975cdb
RouvenGonzalez <rouven.gonzalez@software-developer.org>
2021-03-19 15:04:14 +0100

    fixed router setup to display new about page correctly

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var aboutRouter = require('./routes/about');
 
 var app = express();
 
@@ -22,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/about', aboutRouter);
+app.use('/about', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,13 @@ var router = express.Router();
 /* GET home page. */
 // eslint-disable-next-line no-unused-vars
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'This is the Blog Page' });
+ res.render('index', { title: 'This is the Blog Page' });
+});
+
+/* GET about page. */
+// eslint-disable-next-line no-unused-vars
+router.get('/about', function (req, res, next) {
+ res.render('about', { title: 'This is the About Page' });
 });
 
 module.exports = router;
