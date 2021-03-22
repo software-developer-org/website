79b8cc4d4efadf0e756bd5480c875c1ef9152cfd
KlugeCh <christian.kluge@software-developer.org>
2021-03-22 12:22:32 +0100

    #6 changed rule to 2 space indentation in .prettierrc, run prettier to fix all files

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	preset: 'jest-playwright-preset', testMatch: ['**/__tests__/**/*.+(ts|js)', '**/?(*.)+(e2e.)+(spec|test).+(ts|js)']
+  preset: 'jest-playwright-preset',
+  testMatch: ['**/__tests__/**/*.+(ts|js)', '**/?(*.)+(e2e.)+(spec|test).+(ts|js)'],
 };
-
--- routes/about.js
+++ routes/about.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 // eslint-disable-next-line no-unused-vars
 router.get('/about', function (req, res, next) {
- res.render('about', { title: 'This is the About Page' });
+  res.render('about', { title: 'This is the About Page' });
 });
 
 module.exports = router;
