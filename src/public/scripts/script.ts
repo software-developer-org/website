e1ac832b3f11699417d8368869cdc1c23af52169
RouvenGonzalez <rouven.gonzalez@software-developer.org>
2021-04-09 11:51:31 +0200

    added basic function to script for testing

--- src/public/scripts/script.ts
+++ src/public/scripts/script.ts
@@ -1,1 +1,11 @@
 // test script for hello world
+document.body.onload = addElement;
+
+function addElement () {
+  const newDiv = document.createElement("h1");
+  const newContent = document.createTextNode("Hello");
+  newDiv.appendChild(newContent);
+
+  const currentDiv = document.getElementById("h1");
+  document.body.insertBefore(newDiv, currentDiv);
+}
