3e0c90851c43b7a0a4123ff736633f44a2e3cdc0
RouvenGonzalez <rouven.gonzalez@software-developer.org>
2021-04-09 15:05:52 +0200

    fixed eslint settings and executed npm run format:write

--- build.ts
+++ build.ts
@@ -10,52 +10,46 @@ import childProcess from 'child_process';
 const logger = new Logger();
 logger.timestamp = false;
 
-
-
-
 (async () => {
-    try {
-        // Remove current build
-        await remove('./dist/');
-        // Copy front-end files
-        await copy('./src/public/stylesheets', './dist/public/stylesheets');
-        await copy('./src/views', './dist/views');
-        // Copy back-end files
-        await exec('tsc', './')
-    } catch (err) {
-        logger.err(err);
-    }
+  try {
+    // Remove current build
+    await remove('./dist/');
+    // Copy front-end files
+    await copy('./src/public/stylesheets', './dist/public/stylesheets');
+    await copy('./src/views', './dist/views');
+    // Copy back-end files
+    await exec('tsc', './');
+  } catch (err) {
+    logger.err(err);
+  }
 })();
 
-
 function remove(loc: string): Promise<void> {
-    return new Promise((res, rej) => {
-        return fs.remove(loc, (err) => {
-            return (!!err ? rej(err) : res());
-        });
+  return new Promise((res, rej) => {
+    return fs.remove(loc, (err) => {
+      return !!err ? rej(err) : res();
     });
+  });
 }
-
 
 function copy(src: string, dest: string): Promise<void> {
-    return new Promise((res, rej) => {
-        return fs.copy(src, dest, (err) => {
-            return (!!err ? rej(err) : res());
-        });
+  return new Promise((res, rej) => {
+    return fs.copy(src, dest, (err) => {
+      return !!err ? rej(err) : res();
     });
+  });
 }
 
-
 function exec(cmd: string, loc: string): Promise<void> {
-    return new Promise((res, rej) => {
-        return childProcess.exec(cmd, {cwd: loc}, (err, stdout, stderr) => {
-            if (!!stdout) {
-                logger.info(stdout);
-            }
-            if (!!stderr) {
-                logger.warn(stderr);
-            }
-            return (!!err ? rej(err) : res());
-        });
+  return new Promise((res, rej) => {
+    return childProcess.exec(cmd, { cwd: loc }, (err, stdout, stderr) => {
+      if (!!stdout) {
+        logger.info(stdout);
+      }
+      if (!!stderr) {
+        logger.warn(stderr);
+      }
+      return !!err ? rej(err) : res();
     });
+  });
 }
